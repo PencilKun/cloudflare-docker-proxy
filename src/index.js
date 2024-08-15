a139be53533522cb60d4842e081be1ccd6aeaef5
PencilKun <mdy1124@outlook.com>
2024-08-15 11:03:28 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,15 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.pencilss.top": dockerHub,
+  "quay.pencilss.top": "https://quay.io",
+  "gcr.pencilss.top": "https://gcr.io",
+  "k8s-gcr.pencilss.top": "https://k8s.gcr.io",
+  "k8s.pencilss.top": "https://registry.k8s.io",
+  "ghcr.pencilss.top": "https://ghcr.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.pencilss.top": dockerHub,
 };
 
 function routeByHosts(host) {
